46b651a420565578899b2afe968fd7539f2926b1
Harbor Operations <bfpc.operations@gmail.com>
2025-10-24 01:40:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4134604257-70a73",
+  "appId": "1:151592580585:web:6c3766fa867f628bd50778",
+  "apiKey": "AIzaSyD9qCVJDgw50gb2WlktzVtAnVCkqj5Vaf4",
+  "authDomain": "studio-4134604257-70a73.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "151592580585"
+};
