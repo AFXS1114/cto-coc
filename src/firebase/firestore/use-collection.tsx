cd8445381a00e3c3d1c4c91600181a0661ef3ffa
Harbor Operations <bfpc.operations@gmail.com>
2025-10-30 01:09:07 +0000

    this is my firestore structure..

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -13,7 +13,7 @@ import { errorEmitter } from '@/firebase/error-emitter';
 import { FirestorePermissionError } from '@/firebase/errors';
 
 /** Utility type to add an 'id' field to a given type T. */
-export type WithId<T> = T & { id: string };
+export type WithId<T> = T & { docId: string };
 
 /**
  * Interface for the return value of the useCollection hook.
@@ -78,7 +78,7 @@ export function useCollection<T = any>(
       (snapshot: QuerySnapshot<DocumentData>) => {
         const results: ResultItemType[] = [];
         for (const doc of snapshot.docs) {
-          results.push({ ...(doc.data() as T), id: doc.id });
+          results.push({ ...(doc.data() as T), docId: doc.id });
         }
         setData(results);
         setError(null);
